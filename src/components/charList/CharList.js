f0f96726bded64414678705735729e272edb1197
Broccoli <63042276+HermitBroccoli@users.noreply.github.com>
2022-08-03 00:08:57 +1000

    Initial commit

---
+++ src/components/charList/CharList.js
@@ -0,0 +1,83 @@
+import './charList.scss';
+import abyss from '../../resources/img/abyss.jpg';
+import { Component } from 'react';
+
+import MarvelService from '../../services/MarvelService'
+import Spinner from '../spinner/Spinner'
+import ErrorMessge from '../errorMessage/ErrorMessage'
+
+class CharList extends Component {
+
+	state = {
+		charList: [],
+		loading: true,
+		error: false
+	}
+
+	marvelService = new MarvelService()
+
+	componentDidMount() {
+		this.marvelService.getAllCharacters()
+			.then(this.onCharListLoaded)
+			.catch(this.onError)
+	}
+
+	onCharListLoaded = (charList) => {
+		this.setState({
+			charList,
+			loading: false
+		})
+	}
+
+	onError = () => {
+		this.setState({
+			error: true,
+			loading: false
+		})
+	}
+
+	renderItem(arr) {
+		const items = arr.map((item) => {
+			let imgStyle = { 'objectFit': 'cover' }
+			if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+				imgStyle = { 'objectFit': 'unset' };
+			}
+
+			return (
+				<li className='char__item' key={item.id} onClick={() => this.props.onCharSelected(item.id)}>
+					<img src={item.thumbnail} alt={item.name} style={imgStyle} />
+					<div className='char__name'>{item.name}</div>
+				</li>
+			)
+		})
+		return (
+			<ul className='char__grid'>
+				{items}
+			</ul>
+		)
+	}
+
+	render() {
+
+		const { charList, loading, error } = this.state
+
+		const items = this.renderItem(charList)
+
+		const errorMessage = error ? <ErrorMessge /> : null
+		const spinner = loading ? <Spinner /> : null
+		const content = !(loading && error) ? items : null
+
+		return (
+			<div className="char__list">
+				{errorMessage}
+				{spinner}
+				{content}
+				<button className="button button__main button__long">
+					<div className="inner">load more</div>
+				</button>
+			</div>
+		)
+	}
+}
+
+export default CharList;
